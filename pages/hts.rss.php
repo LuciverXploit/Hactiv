dec06488e8201e8c2a8fc27679138a9e02bb510d
LuciverXploit <lucivergaming22@gmail.com>
2023-12-10 03:32:38 +0700

    hack

---
+++ pages/hts.rss.php
@@ -0,0 +1,71 @@
+<?xml version="1.0" encoding="iso-8859-1" ?>
+<rss version="2.0">
+<channel>
+	<title>HackThisSite - News Articles RSS</title>
+	<description>Hack This Site is a free, safe and legal training ground for hackers to test and expand their hacking skills. More than just 
+another hacker wargames site, we are a living, breathing community with many active projects in development, with a vast selection of hacking 
+articles and a huge forum where users can discuss hacking,
+network security, and just about everything. Tune in to the hacker underground and get involved with the project.</description>
+	<link>https://hackthissite.org</link>
+	<pubDate>Sat, 9 Dec 2023 19:57:59 0</pubDate>
+	<item>
+		<title>Celebrating Pride</title>
+		<link><![CDATA[https://hackthissite.org/news/view/729]]></link>
+		<guid><![CDATA[https://hackthissite.org/news/view/729]]></guid>
+		<description><![CDATA[[center]<b>Wrong format</b>[/center]<br />
+<br />
+At HackThisSite, we have always tried to be a safe, welcoming place for people from every walk of life. This year we would like you to join us in celebrating LGBTQ+ Pride Month for the month of June. Pride is a reminder that while a lot of progress has been made over the past few decades, the fight is far from over and your help is needed.<br />
+<br />
+You may wonder <a href="https://www.huffpost.com/entry/why-pride-an-explanation-for-straight-people_b_5931ccdae4b0649fff2118ee" target="_blank">why people are flying those rainbow flags</a> every year shouting that they are proud. Why do they feel the need to announce it, and <a href="https://www.amnesty.org/en/latest/campaigns/2015/10/5-reasons-why-prides-matter/" target="_blank">why does there need to be a whole month for it</a>? In the perfect world, this would not be necessary.<br />
+<br />
+On June 28, 1969 at the Stonewall Inn in Manhattan, New York, a riot happened. This riot would be the tipping point, starting the path for equality. This was the start of the <a href="https://en.wikipedia.org/wiki/Stonewall_riots" target="_blank">Stonewall Riots</a>, a violent stand against the police criminalizing LGBTQ+ identities. Prid...]]></description>
+		<author>HackThisSite Staff</author>
+		<pubDate>Wed, 2 Jun 2021 14:38:04 0</pubDate>
+	</item>	<item>
+		<title>HackThisSite @ rC3</title>
+		<link><![CDATA[https://hackthissite.org/news/view/727]]></link>
+		<guid><![CDATA[https://hackthissite.org/news/view/727]]></guid>
+		<description><![CDATA[[center]<b>Wrong format</b>[/center]<br />
+<br />
+HackThisSite will be present at the Chaos Computer Congress again this year from December 27 - 30. However, with the ongoing human malware pandemic, CCC will be held entirely remotely and online only as <i>rC3</i>. Therefore, we will be present on both our <a href="ircs://irc.hackthissite.org:7000/#rc3" target="_blank">IRC server (#rc3 channel)</a> and <a href="https://discord.gg/khEkhgJ" target="_blank">Discord (#irc-rc3 channel)</a>, as well as the official rC3 communication platforms (when announced).<br />
+<br />
+Make sure to follow the <a href="https://events.ccc.de" target="_blank">CCC blog</a> and the <a href="https://rc3.world" target="_blank">official rC3 website</a> for more details. The <a href="https://fahrplan.events.ccc.de" target="_blank">schedule of events</a> will soon be posted, and as always, <a href="https://media.ccc.de" target="_blank">talks can be viewed online</a> without a ticket.<br />
+<br />
+See you soon in the virtual world!]]></description>
+		<author>HackThisSite Staff</author>
+		<pubDate>Sun, 6 Dec 2020 22:52:04 0</pubDate>
+	</item>	<item>
+		<title>GO VOTE!</title>
+		<link><![CDATA[https://hackthissite.org/news/view/726]]></link>
+		<guid><![CDATA[https://hackthissite.org/news/view/726]]></guid>
+		<description><![CDATA[[center]<b>Wrong format</b><br />
+&quot;Vote as if your life depends on it - because it does!&quot;[/center]<br />
+<br />
+Those are the famous words of the late and venerable <a href="https://en.wikipedia.org/wiki/Justin_Dart_Jr." target="_blank">Justin Dart Jr.</a>, co-founder of the American Association of People with Disabilities (AAPD) and &#039;Godfather of the Americans with Disabilities Act (ADA)&#039;. He said this long ago to underscore the importance of electing politicians, at all levels, that will be an ally to those less fortunate. Especially those whose lives literally depend on the outcome of an election.<br />
+<br />
+<b>All of our lives hinge on the result of this election in November.</b> The current administration and those that side with it seek to inflict as much pain, suffering, and even death on people they disagree with. That is not hyperbole, that is <a href="https://www.politico.com/news/magazine/2020/08/24/republicanmeltdown-trump-convention-400039" target="_blank">literally their political ideology</a>. From destroying decades of work building civil rights for Black people and LGBTQ+, to empowering and encouraging Nazis and white nationalists, to countless impeachable acts of outright fascism -- and lately...]]></description>
+		<author>HackThisSite Staff</author>
+		<pubDate>Tue, 8 Sep 2020 18:43:32 0</pubDate>
+	</item>	<item>
+		<title>Black Lives Matter</title>
+		<link><![CDATA[https://hackthissite.org/news/view/723]]></link>
+		<guid><![CDATA[https://hackthissite.org/news/view/723]]></guid>
+		<description><![CDATA[[center]<b>Wrong format</b>[/center]<br />
+The unjustifiable murders of George Floyd, Ahmaud Arbery, Breonna Taylor, and so many more have inspired uprisings not just in the United States but across the globe. The world is grieving, and we grieve with them. But <a href="https://www.newyorker.com/news/q-and-a/bryan-stevenson-on-the-frustration-behind-the-george-floyd-protests" target="_blank">the world is also angry</a>, and rightfully so. For far too long, innocent people, especially minorities and persons of color, have been slaughtered by police with utter impunity, and now police are committing outright acts of terrorism against people protesting this injustice, against the press reporting on their heinous actions, and against innocent bystanders and even children that are not participating in any protests. Furthermore, state and federal officials up to and including the White House are using this as an excuse to implement the most egregiously fascist authoritarianism in the US.<br />
+<br />
+This must end <b>now</b>. We must demand justice and accountability and we cannot stop until all police officers, their unions, and police departments are ...]]></description>
+		<author>HackThisSite Staff</author>
+		<pubDate>Thu, 4 Jun 2020 19:19:25 0</pubDate>
+	</item>	<item>
+		<title>HackThisSite @ 36C3</title>
+		<link><![CDATA[https://hackthissite.org/news/view/722]]></link>
+		<guid><![CDATA[https://hackthissite.org/news/view/722]]></guid>
+		<description><![CDATA[[center]<b>Wrong format</b>[/center]<br />
+<br />
+Come meet HackThisSite community members and staff at our <a href="https://36c3.c3nav.de/l/hackthissite/" target="_blank">assembly table</a>, located near Chaos.Jetzt and WikiPaka WG in Hall 2 -- and check out our <a href="https://twitter.com/hackthissite/status/1209610531046219776" target="_blank">blinkenlights</a> while you&#039;re there! You can also chat with us on our IRC channel at <a href="irc://irc.hackthissite.org/#36c3" target="_blank">irc.hackthissite.org #36c3</a>, or <a href="https://irc.hackthissite.org/chat/?join=%2336c3" target="_blank">click here</a> to join IRC by web. We will be competing again this year in the <a href="https://2019.ctf.link" target="_blank">C3 CTF</a>, so stop by our assembly table or join us on IRC if you&#039;d like to join our team (regardless of whether you&#039;re at C3 or not).<br />
+<br />
+If you aren&#039;t able to make it this year, you can always watch the talks <a href="https://streaming.media.ccc.de/36c3/" target="_blank">live online</a> (<a href="https://fahrplan.events.ccc.de/congress/2019/Fahrplan/" target="_blank">schedule here</a>). If you are attending, make sure you <a href="https://events.ccc.de/congress/2019/wiki/index.php/Main_Page" target="_blank">read the wiki</a> to get familiar with this year&#039;s C3.]]></description>
+		<author>HackThisSite Staff</author>
+		<pubDate>Wed, 25 Dec 2019 23:39:24 0</pubDate>
+	</item></channel>
+</rss>
